9504b9167afe95eafcbc37115e32719d7c779906
ildaneta <iildaemanoely@gmail.com>
2021-11-11 23:02:49 -0300

    refactor: Adding .tsx and deleting stylesheet

---
+++ Project_Name/codebase/App.tsx
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
