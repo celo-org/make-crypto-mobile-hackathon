5b5fb1cedb8e5962bb9ae4b87c6d86def728de10
ildaneta <iildaemanoely@gmail.com>
2021-11-15 14:04:48 -0300

    refactor: adding new styles for the containerTextIcon and textButtonDescription

--- Project_Name/codebase/src/components/PillButton/styles.ts
+++ Project_Name/codebase/src/components/PillButton/styles.ts
@@ -1,19 +1,32 @@
 
 import { StyleSheet } from 'react-native';
-import { colors, dimensions, border } from '../../styles';
+import { colors, dimensions, border, fontsFamily, fontsSize } from '../../styles';
 
 export const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: colors.light.neutralColor4,
     height: dimensions.height50,
     width: dimensions.widthFull,
     borderRadius: border.radius.xxl26,
+  },
+
+  containerTextIcon: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    position: 'relative'
   },
   
   icon: {
     marginRight: dimensions.spacingStackLg10
   },
+
+  textButtonDescription: {
+    color: colors.light.neutralColor11,
+    position: 'absolute',
+    right: 20,
+    fontFamily: fontsFamily.montserrat.medium500,
+    fontSize: fontsSize.xs12
+  }
 })
