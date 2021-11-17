7a893cb493a49f8e3051f458fe37e622180af0d4
ildaneta <iildaemanoely@gmail.com>
2021-11-16 22:31:41 -0300

    feat: creating the wallet screen structure

---
+++ Project_Name/codebase/src/screens/Wallet/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+// import { styles } from './styles';
+
+const Wallet = (): JSX.Element => {
+  return (
+    <SafeAreaView>
+      <Text>Wallet screen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Wallet;
