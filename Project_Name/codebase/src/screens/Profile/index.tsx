0c8931e3fbbf2150259717d1c4880b7b12477300
ildaneta <iildaemanoely@gmail.com>
2021-11-16 22:32:35 -0300

    feat: creating the wallet profile structure

---
+++ Project_Name/codebase/src/screens/Profile/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+// import { styles } from './styles';
+
+const Profile = (): JSX.Element => {
+  return (
+    <SafeAreaView>
+      <Text>Profile screen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Profile;
