1d5048372ecfaceb959a993c9b318b1a6281d30e
Joseph Bergeron <jophish126@gmail.com>
2021-11-23 17:39:31 -0500

    Lots of cleaning, updates, features

--- farm-bot/codebase/contracts/farm-bot.sol
+++ farm-bot/codebase/contracts/farm-bot.sol
@@ -2,109 +2,204 @@
 pragma solidity >=0.5.0 <0.9.0;
 
 import "hardhat/console.sol";
-import "./ubeswap-farming/contracts/StakingRewards.sol"; // does the ierc20 import too
+
+import "./ubeswap-farming/contracts/StakingRewards.sol";
 import "./ubeswap/contracts/uniswapv2/interfaces/IUniswapV2Router02.sol";
 import "./ubeswap/contracts/uniswapv2/interfaces/IUniswapV2Pair.sol";
+import "./ubeswap-farming/contracts/Owned.sol";
+import "./FarmbotERC20.sol";
 
-contract FarmBot {
-    mapping(address => uint256) public fpBalances; // maps address to "Farm Point" balances. todo eventually make private
-    // FP balances ONLY changes on deposits/withdrawals, NOT when FarmBot claims rewards. This helps save gas.
-    // NOTE: the thing that DOES change is the VALUE of an FP relative to an LP token
-
+contract FarmBot is Owned, FarmbotERC20 {
     uint256 public lpTotalBalance; // total number of LP tokens owned by Farm Bot
-    uint256 public fpTotalBalance; // total number of "Farm Points" issued by Farm Bot
 
-    IUniswapV2Pair public stakingToken = IUniswapV2Pair(0xe952fe9608a20f80f009a43AEB6F422750285638); // cUSD-CELO
-    StakingRewards public farm = StakingRewards(0x734913751D7390c32410eD2c71Bb1d8210d7570B); // Alfajores farm for cUSD-CELO
-    IERC20 public rewardsToken = IERC20(0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1); // cUSD address
-    IERC20 public celoAddress = IERC20(0xF194afDf50B03e69Bd7D057c1Aa9e10c9954E4C9); // CELO address
-    IUniswapV2Router02 public router = IUniswapV2Router02(0xE3D8bd6Aed4F159bc8000a9cD47CffDb95F96121); // Router address
+    StakingRewards public stakingRewards;
+
+    IERC20 public rewardsToken;
+
+    IUniswapV2Pair public stakingToken; // LP that's being staked
+    IERC20 public stakingToken0; // LP token0
+    IERC20 public stakingToken1; // LP token1
+
+    IUniswapV2Router02 public router; // Router address
+
+    // Paths for swapping; can be updated by owner
+    address[] public path0; // Path to use when swapping rewardsToken to token0. If len < 2, we assume rewardsToken == token0
+    address[] public path1; // Path to use when swapping rewardsToken to token1. If len < 2, we assume rewardsToken == token1
+
+    // Acceptable slippage when swapping/minting LP; can be updated by owner
+    uint256 public slippageNumerator = 99;
+    uint256 public slippageDenominator = 100;
+
+    // Fee awarded to address triggering autocompounding; can be updated by owner
+    uint256 public feeNumerator = 1;
+    uint256 public feeDenominator = 1000; // .1% fee awarded to claimRewards caller
+
+    modifier ensure(uint deadline) {
+        require(deadline >= block.timestamp, 'FarmBot: EXPIRED');
+        _;
+    }
+
+    constructor(
+        address _owner,
+        address _stakingRewards,
+	address _router,
+	address[] memory _path0,
+	address[] memory _path1
+    ) Owned(_owner) {
+        stakingRewards = StakingRewards(_stakingRewards);
+	rewardsToken = stakingRewards.rewardsToken();
+
+	stakingToken = IUniswapV2Pair(address(stakingRewards.stakingToken()));
+	stakingToken0 = IERC20(stakingToken.token0());
+	stakingToken1 = IERC20(stakingToken.token1());
+
+	path0 = _path0;
+	path1 = _path1;
+
+	router = IUniswapV2Router02(_router);
+    }
+
+    function updateFee(uint256 _feeNumerator, uint256 _feeDenominator) external onlyOwner {
+
+    }
+
+    function updatePaths(address[] calldata _path0, address[] calldata _path1) external onlyOwner {
+	path0 = _path0;
+	path1 = _path1;
+    }
+
+    function updateSlippage(uint256 _slippageNumerator, uint256 _slippageDenominator) external onlyOwner {
+	slippageNumerator = _slippageNumerator;
+	slippageDenominator = _slippageDenominator;
+    }
 
     function getFpAmount(uint256 _lpAmount) public view returns (uint256) {
-        // todo check the rounding on this at some point...
         if (lpTotalBalance == 0) {
             return _lpAmount;
         } else {
-            return _lpAmount * fpTotalBalance / lpTotalBalance;
+            return _lpAmount * totalSupply / lpTotalBalance;
         }
     }
 
+    function getLpAmount(uint256 _fpAmount) public view returns (uint256) {
+	if (totalSupply == 0) {
+	    return 0;
+	} else {
+	    return _fpAmount * lpTotalBalance / totalSupply;
+	}
+    }
+
     function deposit(uint256 _lpAmount) public {
-        // todo might need a lock on this
         bool transferSuccess = stakingToken.transferFrom(msg.sender, address(this), _lpAmount);
         require(transferSuccess, "Transfer failed, aborting deposit");
 
         uint256 _fpAmount = this.getFpAmount(_lpAmount);
-        fpTotalBalance += _fpAmount;
+	_mint(msg.sender, _fpAmount);
         lpTotalBalance += _lpAmount;
-        fpBalances[msg.sender] += _fpAmount;
         investInFarm();
+    }
+
+    function withdrawAll() public {
+	require(balanceOf[msg.sender] > 0, "Cannot withdraw zero balance");
+	uint256 _lpAmount = getLpAmount(balanceOf[msg.sender]);
+	withdraw(_lpAmount);
     }
 
     function withdraw(uint256 _lpAmount) public {
         // todo might need a lock on this
         uint256 _fpAmount = this.getFpAmount(_lpAmount);
-        require(fpBalances[msg.sender] >= _fpAmount, "Cannot withdraw more than the total balance of the owner");
+        require(balanceOf[msg.sender] >= _fpAmount, "Cannot withdraw more than the total balance of the owner");
 
         uint256 tokenBalance = stakingToken.balanceOf(address(this));
         if (_lpAmount > tokenBalance) {
-            farm.withdraw(_lpAmount - tokenBalance);
+            stakingRewards.withdraw(_lpAmount - tokenBalance);
         }
 
         bool transferSuccess = stakingToken.transfer(msg.sender, _lpAmount);
         require(transferSuccess, "Transfer failed, aborting withdrawal");
-        fpBalances[msg.sender] -= _fpAmount;
-        fpTotalBalance -= _fpAmount;
+	_burn(msg.sender, _fpAmount);
         lpTotalBalance -= _lpAmount;
     }
 
-    function investInFarm() public {
-        // todo eventually make private. The public function will do more: reap rewards, swap them, obtain LP tokens, and then this.
+    function investInFarm() private {
         uint256 tokenBalance = stakingToken.balanceOf(address(this));
         require(tokenBalance > 0, "Cannot invest in farm because tokenBalance is 0");
-        stakingToken.approve(address(farm), tokenBalance);
-        farm.stake(tokenBalance);
+        stakingToken.approve(address(stakingRewards), tokenBalance);
+        stakingRewards.stake(tokenBalance);
     }
 
-    function claimRewards() public {
+    function claimRewards(uint deadline) public ensure(deadline) {
         // todo eventually make private
-        farm.getReward();
-
+        stakingRewards.getReward();
 	uint256 tokenBalance = rewardsToken.balanceOf(address(this));
+
 	if (tokenBalance == 0) {
 	    return;
 	}
 
-	// Figure out best-case scenario amount of CELO we can get for our cUSD
-	uint256 halfTokens = tokenBalance / 2;
-	uint112 _reserve0;
-	uint112 _reserve1;
-	(_reserve0, _reserve1, ) = stakingToken.getReserves();
-	uint256 amountOut = router.getAmountOut(halfTokens, _reserve0, _reserve1); // Amount of CELO we get for half of our cUSD rewards
+	uint256 feeAmount = tokenBalance * feeNumerator / feeDenominator;
+	uint256 halfTokens = (tokenBalance - feeAmount) / 2;
 
-	// Approve the router to spend our cUSD
-	rewardsToken.approve(address(router), halfTokens);
-	// Swap, accounting for slippage
-	address[] memory path = new address[](2);
-	path[0] = address(rewardsToken);
-	path[1] = address(celoAddress);
-	uint[] memory amounts = router.swapExactTokensForTokens(halfTokens, amountOut*99/100, path, address(this), block.timestamp + 5);
+	uint256 amountToken0;
+	// Figure out best-case scenario amount of token0 we can get and swap
+	if (path0.length >= 2) {
+	    uint[] memory amountsOut = router.getAmountsOut(halfTokens, path0);
+	    uint amountOut = amountsOut[amountsOut.length-1];
+	    rewardsToken.approve(address(router), halfTokens);
+	    uint[] memory amountsSwapped = router.swapExactTokensForTokens(
+                halfTokens,
+		amountOut * slippageNumerator / slippageDenominator,
+		path0,
+		address(this),
+		deadline
+            );
+	    amountToken0 = amountsSwapped[amountsSwapped.length-1];
+	} else {
+	    amountToken0 = halfTokens;
+	}
 
-	uint256 amountCusd = halfTokens; // Amount of cUSD we now have
-	uint256 amountCelo = amounts[1]; // Amount of CELO we now have
+	uint256 amountToken1;
+	// Figure out best-case scenario amount of token1 we can get and swap
+	if (path1.length >= 2) {
+	    uint[] memory amountsOut = router.getAmountsOut(halfTokens, path1);
+	    uint amountOut = amountsOut[amountsOut.length-1];
+	    rewardsToken.approve(address(router), halfTokens);
+	    uint[] memory amountsSwapped = router.swapExactTokensForTokens(
+                halfTokens,
+		amountOut * slippageNumerator / slippageDenominator,
+		path1,
+		address(this),
+		deadline
+            );
+	    amountToken1 = amountsSwapped[amountsSwapped.length-1];
+	} else {
+	    amountToken1 = halfTokens;
+	}
 
-	// Approve the router to spend the farmbot's cUSD/CELO to mint ULP
-	rewardsToken.approve(address(router), amountCusd);
-	celoAddress.approve(address(router), amountCelo);
+	// Approve the router to spend the bot's token0/token1
+	stakingToken0.approve(address(router), amountToken0);
+	stakingToken1.approve(address(router), amountToken1);
 	// Actually add liquidity
-	router.addLiquidity(address(rewardsToken), address(celoAddress), amountCusd, amountCelo, amountCusd * 99 / 100, amountCelo * 99 / 100, address(this), block.timestamp + 5);
+	router.addLiquidity(
+	    address(stakingToken0),
+	    address(stakingToken1),
+	    amountToken0,
+	    amountToken1,
+	    amountToken0 * slippageNumerator / slippageDenominator,
+	    amountToken1 * slippageNumerator / slippageDenominator,
+	    address(this),
+	    deadline
+        );
 
 	// How much LP we have to re-invest
 	uint256 lpBalance = stakingToken.balanceOf(address(this));
-	stakingToken.approve(address(farm), lpBalance);
+	stakingToken.approve(address(stakingRewards), lpBalance);
 
-	// Actually reinvest and adjust the FP weights!!!
-	farm.stake(lpBalance);
+	// Actually reinvest and adjust FP weight
+	stakingRewards.stake(lpBalance);
 	lpTotalBalance += lpBalance;
+
+	// Send incentive fee to sender
+	rewardsToken.transfer(msg.sender, feeAmount);
     }
 }
--- farm-bot/codebase/scripts/deploy.ts
+++ farm-bot/codebase/scripts/deploy.ts
@@ -6,6 +6,15 @@ import { DeployerFn } from '@ubeswap/hardhat-celo';
 // Runtime Environment's members available in the global scope.
 // import { ethers } from "hardhat";
 
+const ALFAJORES_ADDRESS = "0x642abB1237009956BB67d0B174337D76F0455EDd" // Owner
+const STAKING_REWARDS_ADDRESS = "0x734913751D7390c32410eD2c71Bb1d8210d7570B" // StakingRewards
+const ROUTER_ADDRESS = "0xE3D8bd6Aed4F159bc8000a9cD47CffDb95F96121" // Ubeswap Router
+const path0: string[] = [] // path0 should be empty since token0 == rewardsToken == cUSD
+const path1 = [
+  "0x874069fa1eb16d44d622f2e0ca25eea172369bc1", // cUSD
+  "0xf194afdf50b03e69bd7d057c1aa9e10c9954e4c9"  // CELO
+]
+
 import {
   FarmBot__factory
  } from '../../../typechain/factories/FarmBot__factory'
@@ -15,11 +24,17 @@ export const main: DeployerFn<{}> = async ({
   deployer,
   provider
 }) => {
-
-  const farmBot = await deployCreate2('FarmBot_1', {
+  provider
+  const farmBot = await deployCreate2('FarmBot_2', {
     factory: FarmBot__factory,
     signer: deployer,
-    args: [],
+    args: [
+      ALFAJORES_ADDRESS,
+      STAKING_REWARDS_ADDRESS,
+      ROUTER_ADDRESS,
+      path0,
+      path1
+    ],
   })
 
   return {
