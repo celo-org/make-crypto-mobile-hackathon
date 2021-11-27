1493a423c383f59b02be7418a65c6893cb0c750b
ildaneta <iildaemanoely@gmail.com>
2021-11-27 17:19:27 -0300

    feat: creating the walkthrough component and its styles

---
+++ Project_Name/codebase/src/screens/Walkthrough/index.tsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { View, FlatList, Animated } from 'react-native';
+import OnboardingItem from '@nft/components/OnboardingItem';
+import { slides } from '@nft/utils/slides';
+
+import Paginator from '@nft/components/Paginator';
+
+import { styles } from './styles';
+
+const Walkthrough = (): JSX.Element => {
+  const scrollX = useRef(new Animated.Value(0)).current;
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const slidesRef = useRef(null);
+
+  const viewableItemsChanged = useRef(({ viewableItems }: any) => {
+    setCurrentIndex(viewableItems[0].index);
+  }).current;
+
+  const scrollTo = () => {
+    if (currentIndex < slides.length - 1) {
+      slidesRef.current.scrollToIndex({ index: currentIndex + 1 });
+      console.log(currentIndex);
+    }
+  };
+
+  useEffect(() => {
+    console.log(currentIndex);
+  }, [currentIndex]);
+
+  return (
+    <View style={styles.container}>
+      <View style={{ flex: 4, alignItems: 'center', justifyContent: 'center' }}>
+        <FlatList
+          bounces={false}
+          pagingEnabled
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          data={slides}
+          keyExtractor={(item) => item.key}
+          renderItem={({ item }) => (
+            <OnboardingItem image={item.image} title={item.title} description={item.description} />
+          )}
+          onScroll={Animated.event(
+            [
+              {
+                nativeEvent: {
+                  contentOffset: {
+                    x: scrollX,
+                  },
+                },
+              },
+            ],
+            { useNativeDriver: false },
+          )}
+          onViewableItemsChanged={viewableItemsChanged}
+          ref={slidesRef}
+        />
+      </View>
+
+      <Paginator data={slides} scrollX={scrollX} onPress={scrollTo} />
+    </View>
+  );
+};
+
+export default Walkthrough;
---
+++ Project_Name/codebase/src/screens/Walkthrough/styles.ts
@@ -0,0 +1,9 @@
+import { Platform, StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F9F9F9'
+  },
+  
+})
