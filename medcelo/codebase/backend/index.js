03b9970054bdeb07c85ef9fd1fc466f159251c44
iamoracle <officialbilas@gmail.com>
2021-11-28 16:14:00 +0100

    changed contract, improvement

--- medcelo/codebase/backend/index.js
+++ medcelo/codebase/backend/index.js
@@ -1,49 +1,104 @@
-const { Web3Storage } = require('web3.storage')
+const { Web3Storage, getFilesFromPath } = require("web3.storage");
 
-const fileUpload = require('express-fileupload');
+const fileUpload = require("express-fileupload");
 
-const express = require('express')
+const { forceSync } = require("node-force-sync");
 
-const app = express()
+const express = require("express");
 
-const PORT = 8080
+const fs = require("fs");
 
-app.use(fileUpload())
+const app = express();
 
-const token = ''
+const PORT = 8080;
 
-const client = new Web3Storage({ token })
+app.use(fileUpload());
 
-async function storeFiles (files) {
-  const cid = await client.put(files)
+const token =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDg0YTJkMTRhRTIxYzBGZDUzNDlkNkIyN0VhNTZlMTc5ODM5NEE4MGQiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2Mzc0NDE0MjYxMTYsIm5hbWUiOiJtZWRjZWxvIn0.wBq0DP94-PfHyL585R9guBCDsn0OFEuUejGKPRx1wH0";
+
+const client = new Web3Storage({ token });
+
+async function storeFiles(files) {
+  const stream = await getFilesFromPath(files);
+
+  const cid = await client.put(stream);
+
+  return cid;
 }
 
-app.get('/', async (req, res) => {
-    res.send('hello world')
-})
+app.get("/", async (req, res) => {
+  res.send(`<form method="POST" action="" enctype="multipart/form-data">
+    <h2>Upload File</h2>
+    <input type="file" name="thumbnails" multiple>
+    <button type="submit">
+   </form>`);
+});
 
-app.post('/', async (req, res) => {
+app.post("/", (req, res) => {
+  //   storeFiles()
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send("no file uploaded");
+  }
 
-//   storeFiles()
+  const thumbnails = req.files.thumbnails;
 
-    if(!req.files || Object.keys(req.files).length === 0) {
-        res.status(400).send('no file uploaded')
-    }
+  if (!thumbnails) {
+    return res.status(400).send("no thumbnails found");
+  }
 
-    if(!req.files.thumbnail) {
-        res.status(400).send('no thumbnail found')
-    }
+  try {
+    thumbnails.forEach((thumbnail) => {
+      if (
+        !(
+          thumbnail.mimetype == "image/png" ||
+          thumbnail.mimetype == "image/jpeg" ||
+          thumbnail.mimetype == "image/jpg"
+        )
+      ) {
+        throw Error;
+      }
+    });
+  } catch (e) {
+    return res.status(400).send("invalid mimetype");
+  }
 
-    if(!(req.files.thumbnail.mimetype == 'image/png' || req.files.thumbnail.mimetype == 'image/jpeg')) {
-        res.status(400).send('invalid mimetype')
-    }
+  var cids = [];
 
-    await storeFiles(req.files.thumbnail)
+  try {
+    thumbnails.forEach(async function (thumbnail) {
+      const uploadPath = __dirname + "/upload/" + thumbnail.md5 + ".jpeg";
 
+      thumbnail.mv(uploadPath, async function (err) {
+        if (err) {
+          return res.status(500).send(err);
+        }
+      });
 
-    res.send('successfully uploaded!')
-})
+      const syncStoreFiles = forceSync(storeFiles);
+
+      const cid = storeFiles(uploadPath);
+
+      cids.push(cid);
+    });
+
+    Promise.all(cids).then((values) => {
+      thumbnails.forEach(async function (thumbnail) {
+        try {
+          const uploadPath = "./upload/" + thumbnail.md5 + ".jpeg";
+          fs.unlinkSync(uploadPath);
+        } catch (error) {
+          console.log(error);
+        }
+      });
+      res.setHeader("Content-Type", "application/json");
+      return res.send(values);
+    });
+  } catch (error) {
+    return res.status(400).send("couldn't upload file");
+  }
+});
 
 app.listen(PORT, () => {
-  console.log(`Example app listening at http://localhost:${PORT}`)
-})
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
