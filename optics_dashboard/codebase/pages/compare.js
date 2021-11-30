6b8b461a21f1287ba1ec93657887d849f5271f04
Karl Lee <poogooflupduck@gmail.com>
2021-11-30 12:33:53 +0000

    Add optics to Compare

--- optics_dashboard/codebase/pages/compare.js
+++ optics_dashboard/codebase/pages/compare.js
@@ -17,14 +17,24 @@ import {
 } from "@chakra-ui/react";
 import ChakraNextImage from "@/components/ChakraNextImage";
 
+var formatter = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+
+  // These options are needed to round to whole numbers if that's what you want.
+  // minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
+  // maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
+});
+
 export default function Health(props) {
   const [originChain, setOriginChain] = useState("eth");
   const [destinationChain, setDestinationChain] = useState("poly");
+  const [timeframe, setTimeframe] = useState("7days");
 
   return (
     <div>
       <Head>
-        <title>Health - Optics Dashboard</title>
+        <title>Compare - Optics Dashboard</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Nav />
@@ -57,19 +67,72 @@ export default function Health(props) {
             <option value="poly">Polygon</option>
           </Select>
         </Flex>
+        <Text>Average transaction fee</Text>
+        <Stack spacing={3} mt={4}>
+          {props.bridges &&
+            Object.keys(props.bridges).map((index) => {
+              let outflowFee = props.bridges[index][originChain].filter(
+                (entry) =>
+                  entry.DIRECTION == "outflow" && entry.TIMEFRAME == timeframe
+              )[0]["AVG_FEE"];
+              let inflowFee = props.bridges[index][destinationChain].filter(
+                (entry) =>
+                  entry.DIRECTION == "inflow" && entry.TIMEFRAME == timeframe
+              )[0]["AVG_FEE"];
+              let totalFee = outflowFee + inflowFee;
+
+              return (
+                <Box
+                  key={index}
+                  p={6}
+                  borderWidth={1}
+                  borderRadius="md"
+                  display="flex"
+                  alignItems="center"
+                >
+                  {/* <ChakraNextImage
+                  width="2em"
+                  height="2em"
+                  src={
+                    entry.SYMBOL == "WETH"
+                      ? "https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@d5c68edec1f5eaec59ac77ff2b48144679cebca1/svg/color/eth.svg"
+                      : "https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@d5c68edec1f5eaec59ac77ff2b48144679cebca1/svg/color/" +
+                        entry.SYMBOL.toLowerCase() +
+                        ".svg"
+                  }
+                /> */}
+                  <Text pl={2}>{index}</Text>
+                  <Spacer />
+                  {/* <Text>{formatter.format(props.bridges[index][originChain])}</Text> */}
+                  <Text>
+                    {formatter.format(outflowFee) +
+                      " + " +
+                      formatter.format(inflowFee) +
+                      " = " +
+                      formatter.format(totalFee)}
+                  </Text>
+                </Box>
+              );
+            })}
+        </Stack>
       </Page>
     </div>
   );
 }
 
 export async function getStaticProps() {
-  const optics = {
-    eth: await fetch(
-      `https://api.flipsidecrypto.com/api/v2/queries/b0337f8d-66f1-4ab9-bfda-2dc62cce0f75/data/latest`
-    ).then((r) => r.json()),
+  const bridges = {
+    optics: {
+      eth: await fetch(
+        `https://api.flipsidecrypto.com/api/v2/queries/b0337f8d-66f1-4ab9-bfda-2dc62cce0f75/data/latest`
+      ).then((r) => r.json()),
+      poly: await fetch(
+        `https://api.flipsidecrypto.com/api/v2/queries/99173a75-0c4d-48bf-bf5d-7bdcd8925521/data/latest`
+      ).then((r) => r.json()),
+    },
   };
 
   return {
-    props: {}, // will be passed to the page component as props
+    props: { bridges }, // will be passed to the page component as props
   };
 }
